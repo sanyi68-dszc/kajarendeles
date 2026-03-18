85b6dcae67dd70824ad4043c70259f080fec7c1e
Sajat Nev <email@pelda.hu>
2026-03-18 13:50:47 +0100

    Érték kiíratása alert-ben

--- alap.js
+++ alap.js
@@ -1,3 +1,5 @@
 function rendel(){
-    alert("Rendeltél")
+    ertek=parseInt(document.getElementById("kaja").value)
+    if (hazhoz.checked) alert(`${ertek+500} értékben rendeltél házhoz`)
+    else alert(`${ertek} értékben rendeltél`)
 }
